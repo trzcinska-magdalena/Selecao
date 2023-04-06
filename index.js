3a8af51f8f8dc2f57ba1debbac09d0447bd04e90
Magdalena Trzcińska <trzcinskamagdalena01@gmail.com>
2023-04-06 08:18:10 +0200

    fix structure

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
   var $grid = $('.grid').isotope({
     itemSelector: '.grid-item',
     layoutMode: 'masonry'
@@ -9,25 +10,20 @@
   });
 
   var swiper = new Swiper('.swiper', {
-    // Default parameters
     slidesPerView: 1,
     spaceBetween: 50,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-    // Responsive breakpoints
     breakpoints: {
-        // when window width is <= 767px
         768: {
             slidesPerView: 2,
             spaceBetween: 50
         },
-        // when window width is <= 991px
         992: {
             slidesPerView: 3,
             spaceBetween: 50
         }
     }
 });
-
